dd6c3796f18b3f67a869b186e9798c1583acb74a
MarieWalyFall <mariewalyfall@esp.sn>
2024-04-06 13:12:51 +0000

    commit Lab1

---
+++ src/test/java/ipdl/lab0/sbcar/SbCarApplicationTests.java
@@ -0,0 +1,13 @@
+package ipdl.lab0.sbcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
